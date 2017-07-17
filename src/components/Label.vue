a40b0cb73b494553339a7252ea9e09c2528b6492
Manuel Wieser <office@manuelwieser.com>
2017-07-17 19:06:28 +0200

    Use project's logo.

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
+        <img id="source" src="../assets/test.png" style="display: none;">
         <canvas width="256" height="256" v-update-texture="label"></canvas>
-        <label for="input">Aufschrift</label>
-        <input id="input" v-model="label">
+        <label for="labelInput">Aufschrift</label>
+        <input id="labelInput" v-model="label">
     </div>
 </template>
 
@@ -18,6 +19,10 @@
       updateTexture(element, binding) {
         const ctx = element.getContext('2d');
         ctx.clearRect(0, 0, element.width, element.height);
+        const source = document.getElementById('source');
+        if (source && source.complete) {
+          ctx.drawImage(source, 0, 0, element.width, element.height);
+        }
         ctx.fillStyle = 'black';
         ctx.font = '24px Helvetica';
         ctx.textAlign = 'center';
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
 import Label from '@/components/Label';
 
 Vue.use(Router);
@@ -9,10 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
-    }, {
-      path: '/upload',
       name: 'Label',
       component: Label,
     },
