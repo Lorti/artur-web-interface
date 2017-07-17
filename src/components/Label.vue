77c9f1ec7447dd425d9d211e008e8ed8da0defa9
Manuel Wieser <office@manuelwieser.com>
2017-07-17 20:00:46 +0200

    Change texture as soon as its loaded.

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
-        <img id="source" src="../assets/test.png" style="display: none;">
-        <canvas width="256" height="256" v-update-texture="label"></canvas>
-        <label for="labelInput">Aufschrift</label>
-        <input id="labelInput" v-model="label">
-    </div>
+  <div>
+    <img id="source" src="/static/test.png" ref="texture" @load="textureLoaded">
+    <canvas width="256" height="256" v-update-texture="{ texture, label }"></canvas>
+    <label for="labelInput">Aufschrift</label>
+    <input id="labelInput" v-model="label">
+  </div>
 </template>
 
 <script>
@@ -12,29 +12,37 @@
     name: 'label',
     data() {
       return {
+        texture: null,
         label: 'A.R.T.U.R.',
       };
+    },
+    methods: {
+      textureLoaded() {
+        this.texture = this.$refs.texture;
+      },
     },
     directives: {
       updateTexture(element, binding) {
         const ctx = element.getContext('2d');
         ctx.clearRect(0, 0, element.width, element.height);
-        const source = document.getElementById('source');
-        if (source && source.complete) {
-          ctx.drawImage(source, 0, 0, element.width, element.height);
+        if (binding.value.texture) {
+          ctx.drawImage(binding.value.texture, 0, 0, element.width, element.height);
         }
         ctx.fillStyle = 'black';
         ctx.font = '24px Helvetica';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
-        ctx.fillText(binding.value, element.width / 2, element.height / 2);
+        ctx.fillText(binding.value.label, element.width / 2, element.height / 2);
       },
     },
   };
 </script>
 
 <style scoped>
-    canvas {
-        border: 1px solid gray;
-    }
+  canvas {
+    border: 1px solid gray;
+  }
+  #source {
+    display: none;
+  }
 </style>
